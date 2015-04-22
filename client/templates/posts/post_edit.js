5d40f9a7d142510635a5f8760ff3006c1712b0c0
Joe Chan <josephchan91@gmail.com>
2015-04-22 16:59:10 +0800

    chapter 9 - errors

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,16 @@
+Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postEdit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -9,16 +22,21 @@ Template.postEdit.events({
 			title: $(e.target).find('[name=title]').val()
 		};
 
+		var errors = validatePost(postProperties);
+    if (errors.title || errors.url) {
+      return Session.set('postEditErrors', errors);
+    }
+
 		Meteor.call('postUpdate', currentPostId, postProperties, function(error, result) {
 			if (error) {
-				alert(error.reason);
+				return throwError(error.reason);
 			}
 
 			if (result.postExists) {
-				alert('This link has already been posted');
+				return throwError('This link has already been posted');
 			}
 
-			Router.go('postEdit', {_id: currentPostId});
+			Router.go('postPage', {_id: currentPostId});
 		});
 	},
 
